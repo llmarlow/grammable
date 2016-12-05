fdc9253718f1f2e7a8f2aeafd7b6bb3c7678da55
Lorna Marlow <lornamarlow@hotmail.co.uk>
2016-12-05 11:51:02 +0000

    Add gram show page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'grams#index'
-  resources :grams, only: [:new, :create]
+  resources :grams
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
