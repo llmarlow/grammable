24b67d6e6b95d502750062017b2e2339310c4fe9
Lorna Marlow <lornamarlow@hotmail.co.uk>
2016-12-05 12:14:18 +0000

    Add gram update method

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -7,13 +7,14 @@ class GramsController < ApplicationController
   def show
     @gram = Gram.find_by_id(params[:id])
     if @gram.blank?
-      render text: 'Not Found :(', status: :not_found
+      return render_not_found if @gram.blank?
     end
   end
 
   def new
     @gram = Gram.new
   end
+
 
   def create
     @gram = current_user.grams.create(gram_params)
@@ -24,9 +25,32 @@ class GramsController < ApplicationController
     end
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    if @gram.blank?
+      return render_not_found if @gram.blank?
+    end
+  end
+
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.update_attributes(gram_params)
+
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render text: 'Not Found :(', status: :not_found
   end
 end
